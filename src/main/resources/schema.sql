ac3ecdca7a76251fd958eb3fc11263638ed26bde
woowahan-pjs <jason@woowahan.com>
2021-04-28 11:54:46 +0900

    feat(shoppingcart): set up the project

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,57 @@
+drop table if exists orders;
+
+drop table if exists cart;
+
+drop table if exists product;
+
+drop table if exists customer;
+
+create table customer
+(
+    id       bigint       not null auto_increment,
+    username varchar(255) not null,
+    primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+alter table customer
+    add unique key (username);
+
+create table product
+(
+    id    bigint       not null auto_increment,
+    name  varchar(255) not null,
+    price integer      not null,
+    primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+create table cart
+(
+    id          bigint not null auto_increment,
+    customer_id bigint not null,
+    product_id  bigint not null
+);
+
+alter table cart
+    add constraint fk_cart_to_customer
+        foreign key (customer_id) references customer (id);
+
+alter table cart
+    add constraint fk_cart_to_product
+        foreign key (product_id) references product (id);
+
+create table orders
+(
+    id          bigint  not null auto_increment,
+    customer_id bigint  not null,
+    product_id  bigint  not null,
+    quantity    integer not null,
+    primary key (id)
+) engine=InnoDB default charset=utf8mb4;
+
+alter table orders
+    add constraint fk_orders_to_customer
+        foreign key (customer_id) references customer (id);
+
+alter table orders
+    add constraint fk_orders_to_product
+        foreign key (product_id) references product (id);
---
+++ src/test/java/shoppingcart/ApplicationTest.java
@@ -0,0 +1,11 @@
+package shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
